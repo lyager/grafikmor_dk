2a391eaeccc22512e03d73172b9b89aaa4517dfe
Jesper Lyager Nielsen <lyager@gmail.com>
2016-06-30 19:47:50 +0200

    Choices choices

--- go_mor.py
+++ go_mor.py
@@ -3,16 +3,28 @@
 from bottle import route, run, template
 from random import randint
 
+hvad_sir_mor = [
+    'ja',
+    'nej',
+    'mere luft!',
+    'hmmmmmmm'
+]
 
-@route('/hello/<name>')
-def index(name):
-        return template('<b>Hello {{name}}</b>!', name=name)
+
+# @route('/hello/<name>')
+# def index(name):
+#         return template('<b>Hello {{name}}</b>!', name=name)
 
 
 @route('/sir/')
 def index_sir():
     rand = randint(0, 3)
-    return template('<b>Hello {{name}}</b>!', name=rand)
-
+    try:
+        return template(
+            '<b>Grafikmor sir: {{svar}}!!!</b>!', svar=hvad_sir_mor[rand])
+    except IndexError:
+        return template(
+            '<b>Grafikfar skal i skammekrogen, vi fik {{rand}}!!!</b>!',
+            rand=rand)
 
 run(host='localhost', port=8080)
